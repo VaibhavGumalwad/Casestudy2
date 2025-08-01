c2bc44cd1f01f085c5f4345bae1b6061503731b6
VaibhavGumalwad <121026410+VaibhavGumalwad@users.noreply.github.com>
2025-08-01 23:16:27 +0530

    Create main.tf

---
+++ infra/main.tf
@@ -0,0 +1,50 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_security_group" "web" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "web" {
+  ami                    = "ami-0c94855ba95c71c99" # Amazon Linux 2
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.public.id
+  security_groups        = [aws_security_group.web.name]
+  key_name               = "mykey" # Replace with your AWS key pair
+}
+
+resource "aws_eip" "lb" {
+  instance = aws_instance.web.id
+}
